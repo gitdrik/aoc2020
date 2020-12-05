27b49025ebaa18160054eed2750ce4c26a13c764
gitdrik <58667795+gitdrik@users.noreply.github.com>
2020-12-05 12:05:20 +0100

    Add files via upload

--- 05.jl
+++ 05.jl
@@ -1,18 +1,16 @@
 open("05.txt") do f
-    maxID, IDs = 0, []
+    IDs = BitSet() # a sorted set
     for l in eachline(f)
-        l = replace(l, r"F|L"=>'0')
-        l = replace(l, r"B|R"=>'1')
-        ID = parse(Int, l, base=2)
-        maxID = max(ID, maxID)
+        ID = replace(l, r"F|L"=>'0') |>
+             l->replace(l, r"B|R"=>'1') |>
+             l->parse(Int, l, base=2)
         push!(IDs, ID)
     end
-    println("Part 1: ", maxID)
-    sort!(IDs)
-    for i in 2:length(IDs)
-        if IDs[i]-IDs[i-1] > 1
-            println("Part 2: ", IDs[i]-1)
-            break
-        end
+    largest = pop!(IDs)
+    println("Part 1: ", largest)
+    while true
+        nextlargest = pop!(IDs)
+        largest-nextlargest > 1 ? break : largest = nextlargest
     end
+    println("Part 2: ", largest-1)
 end
