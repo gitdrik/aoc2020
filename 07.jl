0783d7a3796b1d49c7f922bb7b0b8030b5ccda25
gitdrik <58667795+gitdrik@users.noreply.github.com>
2020-12-07 19:36:54 +0100

    Cleaned up.

--- 07.jl
+++ 07.jl
@@ -1,40 +1,25 @@
 open("07.txt") do f
     T = Dict()
     for l ∈ eachline(f)
-        outer, inners = split(l, " bags contain ")
-        if inners == "no other bags."
-            T[outer] = Dict()
-        else
-            inners = split(inners, ", ")
-            idict = Dict()
-            for i in inners
+        out, inns = split(l, " bags contain ")
+        T[out] = Dict()
+        if inns != "no other bags."
+            for i in split(inns, ", ")
                 ss = split(i, ' ')
-                idict[string(ss[2],' ',ss[3])] = parse(Int,ss[1])
+                T[out][string(ss[2],' ',ss[3])] = parse(Int,ss[1])
             end
-            T[outer] = idict
         end
     end
 
-    function contains_shiny_gold(T, bag)
-        if "shiny gold" in keys(T[bag])
-            return true
-        else
-            if isempty(T[bag])
-                return false
-            else
-                return any([contains_shiny_gold(T,b) for b in keys(T[bag])])
-            end
-        end
-    end
+    contains_shiny_gold(T, bag) =
+        "shiny gold" in keys(T[bag]) ||
+        any([contains_shiny_gold(T,b) for b in keys(T[bag])])
+
+    bags_in(T, bag) =
+        isempty(T[bag]) ? 0 :
+        sum(values(T[bag])) + sum([bags_in(T,b)*T[bag][b] for b in keys(T[bag])])
+
     println("Part 1: ", sum([contains_shiny_gold(T, bag) for bag in keys(T)]))
+    println("Part 2: ", bags_in(T, "shiny gold"))
 
-    function bags_in(T, bag)
-        if isempty(T[bag])
-            return 0
-        else
-            return sum([bags_in(T,b)*T[bag][b] for b in keys(T[bag])]) +
-                   sum(values(T[bag]))
-        end
-    end
-    println("Part 2: ", bags_in(T, "shiny gold")) #!244
 end
