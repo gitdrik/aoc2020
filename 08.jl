969da352dd4142f229d0da2bc3b52aa23e10f86b
gitdrik <58667795+gitdrik@users.noreply.github.com>
2020-12-08 18:32:43 +0100

    Add files via upload

--- 08.jl
+++ 08.jl
@@ -2,12 +2,11 @@ open("08.txt") do f
     P = []
     for l ∈ eachline(f)
         ins, ns = split(l)
-        n = parse(Int, ns)
-        push!(P, [ins, n])
+        push!(P, [ins, parse(Int, ns)])
     end
 
-    i, seen = 1, Set()
-    acc = 0
+    seen::Set{Int} = Set()
+    i, acc = 1, 0
     while !(i ∈ seen)
         push!(seen, i)
         inc = 1
@@ -21,30 +20,31 @@ open("08.txt") do f
     println("Part 1: ", acc)
 
     for j ∈ 1:length(P)
-        nP = deepcopy(P)
-        if nP[j][1] == "nop"
-            nP[j][1] = "jmp"
-        elseif nP[j][1] == "jmp"
-            nP[j][1] = "nop"
+        org = P[j][1]
+        if org == "nop"
+            P[j][1] = "jmp"
+        elseif org == "jmp"
+            P[j][1] = "nop"
         else
             continue
         end
 
-        i, seen = 1, Set()
-        acc = 0
-        while !(i ∈ seen) && i ∈ 1:length(nP)
+        seen = Set()
+        i, acc = 1, 0
+        while !(i ∈ seen) && i ∈ 1:length(P)
             push!(seen, i)
             inc = 1
-            if nP[i][1] == "acc"
-                acc += nP[i][2]
-            elseif nP[i][1] == "jmp"
-                inc = nP[i][2]
+            if P[i][1] == "acc"
+                acc += P[i][2]
+            elseif P[i][1] == "jmp"
+                inc = P[i][2]
             end
             i += inc
         end
-        if i == length(nP)+1
+        if i == length(P)+1
             println("Part 2: ", acc)
             break
         end
+        P[j][1] == org
     end
 end
