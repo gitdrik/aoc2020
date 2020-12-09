11fbeea399bd0075cfd92505f8b3d2e7aafaa7c8
gitdrik <58667795+gitdrik@users.noreply.github.com>
2020-12-09 07:08:14 +0100

    Add files via upload

--- 09.jl
+++ 09.jl
@@ -1,9 +1,9 @@
 open("09.txt") do f
     ns = [parse(Int, s) for s in eachline(f)]
     p1, p2 = 0, 0
-    for i in 26:length(ns)
+    for i ∈ 26:length(ns)
         valid = false
-        for j in (i-25):(i-1), k in j+1:(i-1)
+        for j ∈ i-25:i-1, k ∈ j+1:i-1
             if ns[i] == ns[j]+ns[k]
                 valid = true
                 break
@@ -14,9 +14,9 @@ open("09.txt") do f
             break
         end
     end
-    for i in 1:length(ns)-1
+    for i ∈ 1:length(ns)-1
         s = ns[i]
-        for j in i+1:length(ns)
+        for j ∈ i+1:length(ns)
             s += ns[j]
             if s == p1
                 p2 = minimum(ns[i:j]) + maximum(ns[i:j])
