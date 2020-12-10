3fe4039dff5f11e108d8969d6f12d95515818fc8
gitdrik <58667795+gitdrik@users.noreply.github.com>
2020-12-10 11:41:25 +0100

    Update 10.jl

--- 10.jl
+++ 10.jl
@@ -1,5 +1,6 @@
 open("10.txt") do f
     ns = [parse(Int, s) for s in eachline(f)]
+    #ns = [16, 10, 15, 5, 1, 11, 7, 19, 6, 12, 4]
     sort!(ns)
     push!(ns,ns[end]+3)
     diffs = ns[2:end].-ns[1:end-1]
