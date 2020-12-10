b26bd76dcb5a2c49df268ea4cb2d0f6b135b95ac
gitdrik <58667795+gitdrik@users.noreply.github.com>
2020-12-10 11:08:05 +0100

    Update 10.jl

--- 10.jl
+++ 10.jl
@@ -2,6 +2,6 @@ open("10.txt") do f
     ns = [parse(Int, s) for s in eachline(f)]
     sort!(ns)
     diffs = ns[2:end].-ns[1:end-1]
-    p1 = length(filter(a->a==1,ns)) * length(filter(a->a==3,ns))
+    p1 = length(diffs[diffs.==1]) * length(diffs[diffs.==3])
     println("Part 1: ", p1)
 end
