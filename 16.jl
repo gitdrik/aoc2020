c4b27441fa5865732188d12a281d38e5f602c0ab
gitdrik <58667795+gitdrik@users.noreply.github.com>
2020-12-16 15:02:54 +0100

    Add files via upload

--- 16.jl
+++ 16.jl
@@ -1,20 +1,20 @@
 open("16.txt") do f
     ls = readlines(f)
     fields = []
-    for l in ls[1:20]
+    for l ∈ ls[1:20]
         r1, r2 = split(l,": ")[2] |> s->split(s," or ") .|>
                  s->split(s, '-') |> ss->parse.(Int, ss) |> r->r[1]:r[2]
         push!(fields, [r1, r2])
     end
     myticket = parse.(Int, split(ls[23],','))
     nearbys = [parse.(Int, split(l,',')) for l in ls[26:end]]
-    p1 = sum([n for n in Iterators.flatten(nearbys) if !any(n .∈ Iterators.flatten(fields))])
+    p1 = sum([n for n ∈ Iterators.flatten(nearbys) if !any(n .∈ Iterators.flatten(fields))])
     println("Part 1: ", p1)
 
     valids = [myticket]
-    for t in nearbys
+    for t ∈ nearbys
         valid = true
-        for n in t
+        for n ∈ t
             if !any(n .∈ Iterators.flatten(fields))
                 valid = false
                 break
@@ -30,14 +30,13 @@ open("16.txt") do f
         tnfs[i,j,k] = any(n .∈ f)
     end
 
-    fm = [sum([all(tnfs[:,n,i]) for n in 1:20]) for i in 1:20]
-    searchorder = sortperm(fm)
-    fieldorder = []
+    searchorder = sortperm([sum([all(tnfs[:,n,i]) for n ∈ 1:20]) for i ∈ 1:20])
+    foundpos = []
     p2 = 1
     for i ∈ searchorder
-        for j in setdiff(Set(1:20), Set(fieldorder))
+        for j ∈ setdiff(Set(1:20), Set(foundpos))
             if all(tnfs[:,j,i])
-                push!(fieldorder, j)
+                push!(foundpos, j)
                 if i ∈ 1:6
                     p2 *= myticket[j]
                 end
