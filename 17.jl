eb376ee01a25d2119a45127f0b435c43dcd21077
gitdrik <58667795+gitdrik@users.noreply.github.com>
2020-12-17 11:37:58 +0100

    Add files via upload

--- 17.jl
+++ 17.jl
@@ -11,14 +11,13 @@ open("17.txt") do f
         for c ∈ 1:n
             nextpocket = deepcopy(pocket)
             for x ∈ 1:xs, y ∈ 1:ys, z ∈ 1:zs
-                neighbors = [(nx,ny,nz) for nx ∈ max(1,x-1):min(xs,x+1)
-                                        for ny ∈ max(1,y-1):min(ys,y+1)
-                                        for nz ∈ max(1,z-1):min(zs,z+1)
-                                               if (nx,ny,nz) != (x,y,z)]
-                activenb = sum([pocket[nb...] for nb in neighbors])
-                if pocket[x,y,z]==1 && activenb ∉ [2,3]
+                neighbors = sum([pocket[nx,ny,nz] for nx ∈ max(1,x-1):min(xs,x+1)
+                                                  for ny ∈ max(1,y-1):min(ys,y+1)
+                                                  for nz ∈ max(1,z-1):min(zs,z+1)
+                                                         if (nx,ny,nz) != (x,y,z)])
+                if pocket[x,y,z]==1 && neighbors ∉ [2,3]
                     nextpocket[x,y,z] = 0
-                elseif pocket[x,y,z]==0 && activenb==3
+                elseif neighbors==3 && pocket[x,y,z]==0
                     nextpocket[x,y,z] = 1
                 end
             end
@@ -32,15 +31,14 @@ open("17.txt") do f
         for c ∈ 1:n
             nextpocket = deepcopy(pocket)
             for x ∈ 1:xs, y ∈ 1:ys, z ∈ 1:zs, w ∈ 1:ws
-                neighbors = [(nx,ny,nz, nw) for nx ∈ max(1,x-1):min(xs,x+1)
-                                            for ny ∈ max(1,y-1):min(ys,y+1)
-                                            for nz ∈ max(1,z-1):min(zs,z+1)
-                                            for nw ∈ max(1,w-1):min(ws,w+1)
-                                              if (nx,ny,nz,nw) != (x,y,z,w)]
-                activenb = sum([pocket[nb...] for nb in neighbors])
-                if pocket[x,y,z,w]==1 && activenb ∉ [2,3]
+                neighbors = sum([pocket[nx,ny,nz,nw] for nx ∈ max(1,x-1):min(xs,x+1)
+                                                     for ny ∈ max(1,y-1):min(ys,y+1)
+                                                     for nz ∈ max(1,z-1):min(zs,z+1)
+                                                     for nw ∈ max(1,w-1):min(ws,w+1)
+                                                       if (nx,ny,nz,nw) != (x,y,z,w)])
+                if pocket[x,y,z,w]==1 && neighbors ∉ [2,3]
                     nextpocket[x,y,z,w] = 0
-                elseif pocket[x,y,z,w]==0 && activenb==3
+                elseif neighbors==3 && pocket[x,y,z,w]==0
                     nextpocket[x,y,z,w] = 1
                 end
             end
