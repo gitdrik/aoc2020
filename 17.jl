10f7e20d99a3aca9696264883b1316e178a6096b
gitdrik <58667795+gitdrik@users.noreply.github.com>
2020-12-17 16:11:47 +0100

    Add files via upload

--- 17.jl
+++ 17.jl
@@ -1,20 +1,23 @@
 open("17.txt") do f
-    pocket3D = BitArray(zeros(20,20,13))
-    pocket4D = BitArray(zeros(20,20,13,13))
+    pocket3D = BitArray(zeros(22,22,15))
+    pocket4D = BitArray(zeros(22,22,15,15))
     for (i,l) in enumerate(eachline(f))
-        pocket3D[7:14,i+6,7] = [c=='#' for c in l]
-        pocket4D[7:14,i+6,7,7] = [c=='#' for c in l]
+        pocket3D[8:15,i+7,8] = [c=='#' for c in l]
+        pocket4D[8:15,i+7,8,8] = [c=='#' for c in l]
     end
 
     function cycle3D(pocket, n)
-        xs, ys, zs = size(pocket)
+        xr, yr, zr = 8:15, 8:15, 8:8
         for c ∈ 1:n
             updates = []
-            for x ∈ 1:xs, y ∈ 1:ys, z ∈ 1:zs
-                neighbors = sum([pocket[nx,ny,nz] for nx ∈ max(1,x-1):min(xs,x+1)
-                                                  for ny ∈ max(1,y-1):min(ys,y+1)
-                                                  for nz ∈ max(1,z-1):min(zs,z+1)
-                                                         if (nx,ny,nz) != (x,y,z)])
+            xr = xr.start-1:xr.stop+1
+            yr = yr.start-1:yr.stop+1
+            zr = zr.start-1:zr.stop+1
+            for x ∈ xr, y ∈ yr, z ∈ zr
+                neighbors = sum([pocket[nx,ny,nz] for nx ∈ x-1:x+1
+                                                  for ny ∈ y-1:y+1
+                                                  for nz ∈ z-1:z+1
+                                          if (nx,ny,nz) != (x,y,z)])
                 if pocket[x,y,z]==1 && neighbors ∉ [2,3]
                     push!(updates, [x,y,z,0])
                 elseif neighbors==3 && pocket[x,y,z]==0
@@ -29,14 +32,18 @@ open("17.txt") do f
     end
 
     function cycle4D(pocket, n)
-        xs, ys, zs, ws = size(pocket)
+        xr, yr, zr, wr = 8:15, 8:15, 8:8, 8:8
         for c ∈ 1:n
             updates = []
-            for x ∈ 1:xs, y ∈ 1:ys, z ∈ 1:zs, w ∈ 1:ws
-                neighbors = sum([pocket[nx,ny,nz,nw] for nx ∈ max(1,x-1):min(xs,x+1)
-                                                     for ny ∈ max(1,y-1):min(ys,y+1)
-                                                     for nz ∈ max(1,z-1):min(zs,z+1)
-                                                     for nw ∈ max(1,w-1):min(ws,w+1)
+            xr = xr.start-1:xr.stop+1
+            yr = yr.start-1:yr.stop+1
+            zr = zr.start-1:zr.stop+1
+            wr = wr.start-1:wr.stop+1
+            for x ∈ xr, y ∈ yr, z ∈ zr, w ∈ wr
+                neighbors = sum([pocket[nx,ny,nz,nw] for nx ∈ x-1:x+1
+                                                     for ny ∈ y-1:y+1
+                                                     for nz ∈ z-1:z+1
+                                                     for nw ∈ w-1:w+1
                                                        if (nx,ny,nz,nw) != (x,y,z,w)])
                 if pocket[x,y,z,w]==1 && neighbors ∉ [2,3]
                     push!(updates, [x,y,z,w,0])
